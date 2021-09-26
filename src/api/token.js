13079f4e7826e649d7b188740fbb1eaca9a0cd59
Pedro <mafuzao@outlook.com>
2021-09-26 20:29:01 -0300

    Tentando consumir api e tentando conexão com mongoose

---
+++ src/api/token.js
@@ -0,0 +1,3 @@
+const apiToken = 'ff2c7b8fe4f3be7085fdd2007b84592a43c04225'
+
+module.exports = apiToken
--- src/config/server.js
+++ src/config/server.js
@@ -4,9 +4,15 @@ const bodyParser= require('body-parser')
 const app = express()
 const port = 3033
 
+
 //parse das requisições via formulário
 app.use(bodyParser.urlencoded({ extended: true }))
 //parse das requisições via json
 app.use(bodyParser.json())
 
+
+
+
 app.listen(port, ()=>console.log(`Servidor rodando na porta ${port}`))
+
+module.exports = app
